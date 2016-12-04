b73849e7d304a727fb05edadbeba6b49f35df5fb
Steven Basileo <DesoGit@users.noreply.github.com>
2016-12-04 00:39:48 -0500

    Add reporting and new back-end parameters

--- chat-plugins/genqueue.js
+++ chat-plugins/genqueue.js
@@ -1,13 +1,12 @@
 'use strict';
 
-//Credits to Mystifi for revamping the system; my old system is commented out down below.
-
 const fs = require('fs');
 const path = require('path');
 
 const requestsFile = path.resolve(__dirname, '../config/chat-plugins/genrequests.json');
 
 const permittedGenners = ['desokoro', 'lombres']
+const permittedGennersByNames = ['Desokoro', 'Lombres']
 
 let genRequests = {requests: [], blacklist: []};
 try {
@@ -16,7 +15,7 @@ try {
 	if (e.code !== 'MODULE_NOT_FOUND') throw e;
 }
 
-if (!(genRequests || typeof genRequests === 'object')) genRequests = {requests: [], blacklist: []};
+if (!(genRequests || typeof genRequests === 'object')) genRequests = {requests: [], blacklist: {}};
 
 function writeGenRequests() {
 	fs.writeFileSync(requestsFile, JSON.stringify(genRequests));
@@ -31,23 +30,112 @@ exports.commands = {
 		writeGenRequests();
 		this.sendReply("Your gen request was successfully added to the queue.");
 	},
-	reqlist: 'reqview',
+
 	reqview: function (target, room, user) {
                 if (user.userid !== 'desokoro') return this.errorReply('You aren\'t permitted to use this command.');
 		//if (!this.can('hotpatch')) return false;
 		if (!genRequests.requests.length) return this.errorReply('There are no current genning requests.');
 		let output = `There are <b>${genRequests.requests.length}</b> users who requested genning:<br />`;
 		for (let i = 0; i < genRequests.requests.length; i++) {
-			output += `<b>${genRequests.requests[i].username}</b>: ${genRequests.requests[i].request} (${genRequests.requests[i].time}).<br />`;
+			output += `<b>${genRequests.requests[i].username}</b>: ${genRequests.requests[i].request} (${genRequests.requests[i].time}).`;
 		}
 		this.sendReplyBox(output);
 	},
-
 
         gbl: 'genblacklist',
 	genblacklist: function (target, room, user) {
                 if (user.userid !== 'desokoro') return this.errorReply('You aren\'t permitted to use this command.');
 		//if (!this.can('hotpatch')) return false;
+		if (!target) return this.errorReply('Please specify a target.');
+		let blacklisted = toId(target);
+		if (genRequests.blacklist.includes(blacklisted)) return this.errorReply(`${target} is already present on the genning blacklist.`);
+		genRequests.blacklist.push(blacklisted);
+		writeGenRequests();
+		this.sendReply(`${target} was added to the genning blacklist.`);
+		// Notify the user that they were blacklisted, if they are online.
+		if (Users(blacklisted)) Users(blacklisted).popup(`You were added to the genning blacklist by ${user.name}. If you feel this is unfair, please PM an administrator (~) or the user who blacklisted you.`);
+	},
+
+        gwl: 'genunblacklist',
+        genwhitelist: 'genunblacklist',
+	genunblacklist: function (target, room, user) {
+                if (user.userid !== 'desokoro') return this.errorReply('You aren\'t permitted to use this command.');
+		//if (!this.can('hotpatch')) return false;
+		if (!target) return this.errorReply("Please specify a target.");
+		let blacklisted = toId(target);
+		if (!genRequests.blacklist.includes(blacklisted)) return this.errorReply(`${target} is currently not on the genning blacklist.`);
+		genRequests.blacklist.splice(genRequests.blacklist.indexOf(blacklisted), 1);
+		writeGenRequests();
+		this.sendReply(`${target} was removed from the genning blacklist.`);
+		// Notify the user that they were unblacklisted, if they are online.
+		if (Users(blacklisted)) Users(blacklisted).popup(`You were removed from the genning blacklist by ${user.name}.`);
+	},
+
+        vbl: 'blacklistview',
+        blv: 'blacklistview',
+	blacklistview: function (target, room, user) {
+                if (user.userid !== 'desokoro') return this.errorReply('You aren\'t permitted to use this command.');
+		//if (!this.can('hotpatch')) return false;
+		if (!genRequests.blacklist.length) return this.errorReply("There are currently no users on the genning blacklist.");
+		let output = `There are <b>${genRequests.blacklist.length}</b> users on the genning blacklist:<br />`;
+		for (let i = 0; i < genRequests.blacklist.length; i++) {
+			output += `<i>${genRequests.blacklist[i]}</i><br />`;
+		}
+		this.sendReplyBox(output);
+	},
+
+        delreq: 'reqclear',
+	reqclear: function (target, room, user) {
+                if (user.userid !== 'desokoro') return this.errorReply('You aren\'t permitted to use this command.');
+		//if (!this.can('hotpatch')) return false;
+		if (!(target || target === 'force')) {
+			this.errorReply("WARNING: This will reset all genning requests on the server.");
+			this.errorReply("If you wish to continue, use /reqclear force.");
+			return;
+		}
+		// Really lazy hack for doing this - just reset everything in genRequests.requests.
+		genRequests.requests = [];
+		writeGenRequests();
+		this.sendReply("All genning requests were erased from the server.");
+	},
+
+	genguide: function (target, room, user) {
+	this.sendReplyBox('<center><h3>GENNING GUIDE</h3></center><center><h4>genreq (request): Requests a gen for those who can gen and are permitted by Tsunami to do so</h4></center><center><h4>viewgenners: View eligible genners</h4></center><center><h4>reportgenner: Reports a genner to Desokoro; use only if absolutely needed</h4></center><center><h2>More commands available to all users will be listed here in the future</h2></center>');
+	},
+
+	staffgenguide: function (target, room, user) {
+	if (!permittedGenners.includes(user.userid)) return this.errorReply('You are not permitted to view this.');
+	this.sendReplyBox('<center><h3>GENNING RESPONSE GUIDE</h3></center><center></h1>reqclear: Clears all requests</h1></center><center></h1>vbl: Views all blacklisted users</h1></center><center></h1>gbl: Blacklists a user.</h1></center><center></h1>gwl: Un-blacklists a user</h1></center>');
+	},
+
+	genneradd: function (target, room, user) {
+	if (user.userid !== 'desokoro') return this.errorReply('You are not permitted to use this command.');
+	if (!target) return this.errorReply('Please specify a target.')
+	let targetUser = Users(toId(target));
+	if (!targetUser) return this.errorReply('The target user ' + target + ' could not be found. Please check your spelling!');
+	if (permittedGenners.includes(targetUser.userid)) return this.errorReply('The target user ' + target + ' is already on the permitted genners list.');
+	permittedGenners.push(targetUser.userid);
+	permittedGennersByNames.push(targetUser.name);
+	this.sendReply('You have successfully added ' + targetUser.name + ' to the permitted genners list!');
+	console.log(targetUser.name + ' was added to the permitted genners list by ' + user.name + '.');
+	},
+
+	viewgenners: function (target, room, user) {
+	for (let i = 0; i <  permittedGenners.length; i++) { this.sendReplyBox(permittedGenners[i]);   }
+	},
+
+	reportgenner: function (target, room, user) {
+	//let CaseSensitiveArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	if (genRequests.blacklist.includes(user.userid)) return this.errorReply('You are not allowed to partake in any genning services.');
+	if (user.canTalk) return this.errorReply('You are currently unable to be trusted due to your disciplinary status; your report will not be accepted.');
+	if (!user.autoconfirmed) return this.errorReply('You are not autoconfirmed and your report will not be accepted.');
+	if (!target) return this.errorReply('Please specify a target.');
+	if (!permittedGenners.includes(target) && !permittedGennersByNames.includes(target)) return this.errorReply('The user you have reported is not a valid genner.');
+	if (target === 'desokoro') return this.errorReply('Reporting the owner of the server to himself won\'t do you much good...');
+	//if (CaseSensitiveArray.includes(target)) this.errorReply('Only lowercase letters and numbers can be used to report a genner.');
+	this.parse('/tell Desokoro, TOKEN OF AUTHENTICITY: ' + Math.floor(Math.random() * 20) + '. Reporting genner ' + target + ' for misconduct.');
+	},
+};
 		if (!target) return this.errorReply('Please specify a target.');
 		let blacklisted = toId(target);
 		if (genRequests.blacklist.includes(blacklisted)) return this.errorReply(`${target} is already present on the genning blacklist.`);
