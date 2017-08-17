78891188b2e9cace1b276ad8886face0f694f196
Steven Basileo <DesoGit@users.noreply.github.com>
2017-08-17 12:59:14 -0400

    moves.js
    probably broken

---
+++ mods/desometa/moves.js
@@ -0,0 +1,67 @@
+'use strict';
+
+exports.BattleMovedex = {
+	"Industrialize": {
+		num: 678,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "Steel and Poison type moves do double damage. Grass and Bug moves do half. Lasts 5 turns (8 with Oil Canister). Autotomize has double effectiveness",
+		shortDesc: "Steel/Poison x2 damage, Grass/Bug 0.5x damage, 5 turns (8 w Oil Canister), Autotomize is 2x more effective",
+		id: "industrialize",
+		name: "Industrialize",
+		pp: 10,
+		priority: 0,
+		flags: {nonsky: 1},
+		terrain: 'Industrialized',
+		effect: {
+			duration: 3,
+			durationCallback: function (source, effect) {
+				if (source && source.hasItem('Oil Canister')) {
+					return 8;
+				}
+				return 5;
+			},
+			onModifyAtkPriority: 5,
+			onModifyAtk: function (atk, pokemon) {
+      if (move.type === 'Steel' || move.type === 'Poison') {
+				return this.chainModify(2);
+        }
+      if (move.type === 'Grass' || move.type === 'Bug') {
+        return this.chainModify(0.5);
+        }
+			},
+			onModifySpAPriority: 5,
+			onModifySpA: function (spa, pokemon) {
+      if move.type === 'Steel || move.type === 'Poison') {
+				return this.chainModify(2);
+        }
+      if (move.type === 'Grass' || move.type === 'Bug') {
+        return this.chainModify(0.5);
+			},
+      onModifySpePriority: 5,
+      onModifySpe: function (spe, pokemon) {
+      if (move.id === 'autotomize') {
+      return this.chainModify(3);
+      }
+      },
+			onStart: function (battle, source, effect) {
+				if (effect && effect.effectType === 'Ability') {
+					this.add('-fieldstart', 'move: Industrialized', '[from] ability: ' + effect, '[of] ' + source);
+				} else {
+					this.add('-fieldstart', 'move: Industrialized');
+				}
+			},
+			onResidualOrder: 21,
+			onResidualSubOrder: 2,
+			onEnd: function () {
+				this.add('-fieldend', 'move: Industrialized');
+			},
+		},
+		secondary: false,
+		target: "all",
+		type: "Poison",
+		zMoveBoost: {spa: 1},
+		contestType: "Clever",
+	},
+};
