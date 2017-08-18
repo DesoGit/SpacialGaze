b73c44b8739f307f33ef97e4d8f880ccd940bb1e
Steven Basileo <DesoGit@users.noreply.github.com>
2017-08-18 12:05:00 -0400

    Fix Oil Canister description

--- mods/desometa/items.js
+++ mods/desometa/items.js
@@ -21,7 +21,7 @@ exports.BattleItems = {
 		},
 		num: -700,
 		gen: -1,
-		desc: "Holder's use of Industrialized lasts 8 turns instead of 5, and holder takes boosts Speed of Steel types by 1.5x, and Fire type moves do 2x the damage if held.",
+		desc: "Holder's use of Industrialized lasts 8 turns instead of 5. Steel types have 1.5x speed by holding this. All Pokemon who hold this take 2x damage from Fire attacks.",
 		shortDesc: "If the user holds this: Industrialized lasts 8 turns, Steel types' Speed gets boosted by 1.5x, Fire type moves do 2x damage on holder.",
 	},
 };
