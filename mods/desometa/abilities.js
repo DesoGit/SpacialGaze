11ed1747c0f0902131e86cdf5af3512f00f93229
returningavenger <miguelmayers2001@gmail.com>
2017-08-18 14:52:34 +0000

    DesoMeta: Adjust Industrialized Mechanics

--- mods/desometa/abilities.js
+++ mods/desometa/abilities.js
@@ -4,59 +4,51 @@ exports.BattleAbilities = {
 	hallucinogen: {
 		id: "hallucinogen",
 		name: "Hallucinogen",
-		shortDesc: "On switch-in, this Pokemon causes Hallucinogen.",
-		onStart: function (source) {
-			this.setTerrain('hallucinogen');
-		},
-		rating: 5,
-	},
-	industrialized: {
-		id: "industrialized",
-		name: "Industrialized",
-<<<<<<< HEAD
-		desc: "This Pokemon's Steel & Poison type moves do 2x damage. This Pokemon's Grass & Bug type moves do 0.5x damage. Autotomize is doubled in effectiveness.",
+		desc: "Max Attack, Special Attack and Speed for three turns in exchange for 0.25x accuracy and all moves become Grass type. All attacks that connect confuse both players.",
 		onStart: function (pokemon) {
-			pokemon.addVolatile('industrialized');
-			this.add('', 'The battlefield has been industrialized!');
+			pokemon.addVolatile('hallucinogen');
+			this.add('', 'The battlefield has suddenly become trippy as hell!');
 		},
 		onEnd: function (pokemon) {
-			delete pokemon.volatiles['industrialized'];
-			this.add('-end', pokemon, 'Industrialized', '[silent]');
-			this.add('', 'The battlefield has become Detroit!');
+			delete pokemon.volatiles['hallucinogen'];
+			this.add('-end', pokemon, 'Hallucinogen', '[silent]');
+			this.add('', 'The battlefield has lost its trippiness.');
 		},
 		effect: {
 			duration: 4,
 			onStart: function (target) {
-				this.add('-start', target, 'ability: Industrialized');
+				this.add('-start', target, 'ability: Hallucinogen');
 			},
 			onModifyAtkPriority: 5,
-			onModifyAtk: function (atk, pokemon, move) {
-				if (move.type === 'Steel' || move.type === 'Poison') {
-					return this.chainModify(2);
-				} else if (move.type === 'Grass' || move.type === 'Bug') {
-					return this.chainModify(0.5);
-				}
+			onModifyAtk: function (atk, pokemon) {
+				return this.chainModify(4);
+			},
+			onModifySpePriority: 5,
+			onModifySpe: function (spe, pokemon) {
+				return this.chainModify(4);
 			},
 			onModifySpAPriority: 5,
-			onModifySpA: function (spa, pokemon, move) {
-				if (move.type === 'Steel' || move.type === 'Poison') {
-					return this.chainModify(2);
-				} else if (move.type === 'Grass' || move.type === 'Bug') {
-					return this.chainModify(0.5);
-				}
+			onModifySpA: function (spa, pokemon) {
+				return this.chainModify(4);
+			},
+			onSourceModifyAccuracy: function (accuracy) {
+				if (typeof accuracy !== 'number') return;
+				return accuracy * 0.25;
 			},
 			onModifyMovePriority: 1,
 			onModifyMove: function (move) {
-				if (move.id === 'autotomize') {
-					return this.chainModify(3);
+				if (move.id !== 'struggle' && this.getMove(move.id).type !== 'Grass') {
+					move.type = 'Grass';
 				}
 			},
 			onEnd: function (target) {
-				this.add('-end', target, 'Industrialized');
+				this.add('-end', target, 'Hallucinogen');
 			},
 		},
-		rating: 5,
-=======
+	},
+	industrialized: {
+		id: "industrialized",
+		name: "Industrialized",
 		desc: "On switch-in, the user summons Industrialized terrain.",
 		onStart: function (pokemon) {
 			this.setTerrain('industrialized');
@@ -65,6 +57,5 @@ exports.BattleAbilities = {
 		onEnd: function (pokemon) {
 			this.add('', 'The battlefield has become Detroit!');
 		},
->>>>>>> 990bdf49d16268834003241b5bd625dce05f5498
 	},
 };
--- mods/desometa/moves.js
+++ mods/desometa/moves.js
@@ -1,55 +1,13 @@
 'use strict';
 
 exports.BattleMovedex = {
-<<<<<<< HEAD
-	"hallucinogen": {
-=======
 	"industrialize": {
->>>>>>> 990bdf49d16268834003241b5bd625dce05f5498
 		num: 678,
 		accuracy: true,
 		basePower: 0,
 		category: "Status",
-<<<<<<< HEAD
-		desc: "Max Attack, Special Attack and Speed for three turns in exchange for 0.25x accuracy and all moves become Grass type. All attacks that connect confuse both players. Lasts 5 turns if the user is holding Terrain Extender.",
-		shortDesc: "Max Atk, SpA & Spe for 3 turns, 0.25x accuracy, all moves = Grass. All attacks that hit, confuse both players.",
-		id: "hallucinogen",
-		name: "Hallucinogen",
-		pp: 10,
-		priority: 0,
-		flags: {nonsky: 1},
-		terrain: 'hallucinogen',
-		effect: {
-			duration: 3,
-			durationCallback: function (source, effect) {
-				if (source && source.hasItem('terrainextender')) {
-					return 5;
-				}
-				return 3;
-			},
-			onModifyAtkPriority: 5,
-			onModifyAtk: function (atk, pokemon) {
-				return this.chainModify(4);
-			},
-			onModifySpePriority: 5,
-			onModifySpe: function (spe, pokemon) {
-				return this.chainModify(4);
-			},
-			onModifySpAPriority: 5,
-			onModifySpA: function (spa, pokemon) {
-				return this.chainModify(4);
-			},
-			onSourceModifyAccuracy: function (accuracy) {
-				if (typeof accuracy !== 'number') return;
-				return accuracy * 0.25;
-			},
-			onModifyMovePriority: 1,
-			onModifyMove: function (move) {
-				if (move.id !== 'struggle' && this.getMove(move.id).type !== 'Grass') {
-					move.type = 'Grass';
-=======
 		desc: "Steel and Poison type moves do double damage. Grass and Bug moves do half. Lasts 5 turns (8 with Oil Canister). Autotomize has double effectiveness",
-		shortDesc: "Steel/Poison x2 damage, Grass/Bug 0.5x damage, 5 turns (8 w Oil Canister), Autotomize is 2x more effective",
+		shortDesc: "Steel/Poison x2 damage, Grass/Bug 0.5x damage, 5 turns (8 w/ Oil Canister), Autotomize is 2x more effective",
 		id: "industrialize",
 		name: "Industrialize",
 		pp: 10,
@@ -59,7 +17,7 @@ exports.BattleMovedex = {
 		effect: {
 			duration: 3,
 			durationCallback: function (source, effect) {
-				if (source && source.hasItem('Oil Canister')) {
+				if (source && source.hasItem('oilcanister')) {
 					return 8;
 				}
 				return 5;
@@ -84,30 +42,19 @@ exports.BattleMovedex = {
 			onModifySpe: function (spe, pokemon, move) {
 				if (move.id === 'autotomize') {
 					return this.chainModify(3);
->>>>>>> 990bdf49d16268834003241b5bd625dce05f5498
 				}
 			},
 			onStart: function (battle, source, effect) {
 				if (effect && effect.effectType === 'Ability') {
-<<<<<<< HEAD
-					this.add('-fieldstart', 'move: Hallucinogen', '[from] ability: ' + effect, '[of] ' + source);
-				} else {
-					this.add('-fieldstart', 'move: Hallucinogen');
-=======
 					this.add('-fieldstart', 'move: Industrialized', '[from] ability: ' + effect, '[of] ' + source);
 				} else {
 					this.add('-fieldstart', 'move: Industrialized');
->>>>>>> 990bdf49d16268834003241b5bd625dce05f5498
 				}
 			},
 			onResidualOrder: 21,
 			onResidualSubOrder: 2,
 			onEnd: function () {
-<<<<<<< HEAD
-				this.add('-fieldend', 'move: Hallucinogen');
-=======
 				this.add('-fieldend', 'move: Industrialized');
->>>>>>> 990bdf49d16268834003241b5bd625dce05f5498
 			},
 		},
 		secondary: false,
@@ -116,8 +63,4 @@ exports.BattleMovedex = {
 		zMoveBoost: {spa: 1},
 		contestType: "Clever",
 	},
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 990bdf49d16268834003241b5bd625dce05f5498
