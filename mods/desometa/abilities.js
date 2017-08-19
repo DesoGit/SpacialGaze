1c9c2cf0d65bcc34917a575008d58d9efb3b2d98
Steven Basileo <DesoGit@users.noreply.github.com>
2017-08-19 19:16:27 -0400

    Update abilities.js

--- mods/desometa/abilities.js
+++ mods/desometa/abilities.js
@@ -8,12 +8,12 @@ exports.BattleAbilities = {
 		shortDesc: "4x Atk, SpA & Spe for 3 turns, but 0.25x Accuracy.  All moves become Grass type.",
 		onStart: function (pokemon) {
 			pokemon.addVolatile('hallucinogen');
-			this.add('', pokemon, ' has suddenly started tripping.');
+			this.add('-message', pokemon + ' has suddenly started tripping.');
 		},
 		onEnd: function (pokemon) {
 			delete pokemon.volatiles['hallucinogen'];
 			this.add('-end', pokemon, 'Hallucinogen', '[silent]');
-			this.add('', pokemon, ' has stopped tripping.');
+			this.add('-message', pokemon + ' has stopped tripping.');
 		},
 		effect: {
 			duration: 3,
